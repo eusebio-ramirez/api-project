c82d51d10a57a63c2a5f4a7b75df04132fcdb66a
eusebio-ramirez <e.ramirez@industrial-code.com>
2024-12-16 23:00:48 -0600

    Rubros

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const menusRoutes = require("./routes/menu");
+const agendaRoutes = require("./routes/agenda");
+const rubrosRoutes = require("./routes/rubros");
 
-const menusRoutes = require('./routes/menu');
-const agendaRoutes = require('./routes/agenda');
-
-
-
-app.use('/api/menu', menusRoutes);
-app.use('/api/agenda', agendaRoutes);
-
+app.use("/api/menu", menusRoutes);
+app.use("/api/agenda", agendaRoutes);
+app.use("/api/rubros", rubrosRoutes);
 
 // Iniciar el servidor
 const PORT = 3000;
